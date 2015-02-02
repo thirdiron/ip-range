76d51c4630715d870d3eb44bc9f5364acf1bfe96
Mike Lang <mike.lang1010@gmail.com>
2015-02-02 14:31:29 -0600

    Added tests on cidr notation conversion

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var ip = require('./');
-
+var util = require('util');
 
 var range = ip.range('192.168.0.1');
 
@@ -8,6 +8,8 @@ assert(range.valid, range.errors);
 assert(range.contains('192.168.0.1'));
 assert(!range.contains('192.168.0.0'));
 
+assert.equal(ip.range("0-255.1.1.1").errors, "Hyphenated values not allowed in first or second octet. ");
+assert.equal(ip.range("1.0-255.1.1").errors, "Hyphenated values not allowed in first or second octet. ");
 assert.equal(ip.range("1.2.3").errors, "IP must contain 4 octets, contained: 3");
 assert.equal(ip.range("1.2.3.a").errors, "error in octet 4, invalid octet spec: 'a'");
 assert.equal(ip.range("1.2.3.266").errors, "error in octet 4, octet range out of bounds: '266'");
@@ -18,6 +20,34 @@ assert(!ip.range('1.0.1.*').contains('1.0.0.10'));
 assert(ip.range('1.2.3.4-10').contains('1.2.3.4', '1.2.3.10'));
 assert(ip.range('1.2.3.5', '1.2.3.4').contains('1.2.3.4', '1.2.3.5'));
 
-// Must add tests for inetAddresses property!
-assert(false);
+// Handy tool for generating test data: http://www.ipaddressguide.com/cidr#range
+// If the range finishes with wildcards, we should have
+// one cidr notation range with a netmask.
+assert(ip.range('1.0.1.*').inetAddresses.length === 1);
+assert(ip.range('1.0.1.*').inetAddresses[0] === '1.0.1.0/24');
 
+assert(ip.range('1.0.*.*').inetAddresses.length === 1);
+assert(ip.range('1.0.*.*').inetAddresses[0] === '1.0.0.0/16');
+
+// Ranges that don't align on powers of 2 boundaries will require
+// multiple cidr notation ranges to specify.
+
+// Our package leans upon the ip-subnet-calculator module
+// to do the heavy lifting of calculating cidr notation ranges.
+// The current version over-specifies some ranges, but not so
+// much as to cause us preformance problems.  We can leave
+// eliminating redundant ranges as a possible opportunity for
+// improvement.
+assert(ip.range('1.2.3.4-10').inetAddresses.length === 3);
+assert(ip.range('1.2.3.4-10').inetAddresses.indexOf('1.2.3.4/30') !== -1);
+assert(ip.range('1.2.3.4-10').inetAddresses.indexOf('1.2.3.8/31') !== -1);
+
+// Non-contiguous ranges need multiple cidr notation ranges
+// to specify as well
+assert(ip.range('1.2.0-5.0').inetAddresses.length === 6);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.0.0/32') !== -1);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.1.0/32') !== -1);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.2.0/32') !== -1);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.3.0/32') !== -1);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.4.0/32') !== -1);
+assert(ip.range('1.2.0-5.0').inetAddresses.indexOf('1.2.5.0/32') !== -1);
